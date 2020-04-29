b7ce0e3be0054574a876031b39b1a61311712aa5
jvanam <jessicavanam@gmail.com>
2020-04-29 13:52:21 -0700

    add quiz image interactivity on click

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,7 +1,8 @@
 var btn = document.querySelector("#b-col1-item");
 var quizFrame = document.querySelector("#q-grid-container");
-var quizState = false;
+var qImg = document.querySelectorAll(".q-img");
 
+// toggle the quiz module
 btn.onclick = () => {
   if (quizFrame.style.display === "grid") {
     quizFrame.style.display = "none";
@@ -9,3 +10,11 @@ btn.onclick = () => {
     quizFrame.style.display = "grid";
   }
 };
+
+// add event listeners to quiz squares
+ qImg.forEach((el) => {
+   el.addEventListener("click", function(){
+     el.style.margin = "10px";
+     el.style.backgroundSize="106px";
+   });
+ });
