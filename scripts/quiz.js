ebc2c473a35c9ec4eafa882ae610f649541db8ad
jvanam <jessicavanam@gmail.com>
2020-05-06 14:38:45 -0700

    update scores to database, update header style

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,17 +1,8 @@
-// TODO:
-// add var questionid = '' upon init
-// add click ventlistener to verify BUTTON
-// onclick it will:
-// find the imgkey associated w each clicked button and add one to its value
-// reset the questionid
-// re-render
 var initBtn = document.querySelector("#b-col1-item");
 var quizFrame = document.querySelector("#q-grid-container");
 var qImg = document.querySelectorAll(".q-img");
 var verBtn = document.querySelector("#verify-button");
-// for each quiz square element, add a state (un/clicked) and question/img key
-var squaresDict = {"img01":false,"img02":false,"img03":false,"img04":false,
-"img05":false,"img06":false,"img07":false,"img08":false,"img09":false};
+
 // Get a reference to the database service
 var database = firebase.database();
 //set the level and question vars
@@ -48,27 +39,39 @@ function populateSquares() {
       // failed
     });
 }
-
 populateSquares();
 
-
 // add event listeners to quiz squares
-// so far, only for click and only for type1 and type2
  qImg.forEach((el) => {
    el.addEventListener("click", function(){
-     if(el.style.margin === "10px"){
+     if(el.style.margin === "12px"){
        el.style.margin = "0px";
        el.style.backgroundSize="126px";
      }
      else{
-       el.style.margin = "10px";
+       el.style.margin = "12px";
        el.style.backgroundSize="106px";
      }
    });
  });
 
-// verify BUTTON
+// verify button
 verBtn.addEventListener("click", function(){
+  qImg.forEach((el) => {
+    if(el.style.margin === "10px"){
+      key = el.id
+      // Increment the img score by 1.
+      var postRef = firebase.database().ref(lvl+'/scores/q'+qst.toString()+"/"+key);
+      postRef.transaction(function(updateScore) {
+        return updateScore + 1;
+      });
+      console.log(key);
+      el.style.margin = "0px";
+      el.style.backgroundSize="126px";
+    }
+  });
   qst += 1;
   populateSquares();
 })
+
+// DANGER ZONE! RESET ALL SCORES TO ZERO //
