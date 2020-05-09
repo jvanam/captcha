605b4572b5c2b0d8e5c65a84a5e330daf664e9c6
jvanam <jessicavanam@gmail.com>
2020-05-09 14:55:49 -0700

    retrieve lvl1 and lvl2 image links outside of populateSquares()

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,3 +1,6 @@
+// Get a reference to the database service
+var database = firebase.database();
+// CSS selectors
 var initBtn = document.querySelector("#b-col1-item");
 var quizFrame = document.querySelector("#q-grid-container");
 var qImg = document.querySelectorAll(".q-img");
@@ -5,57 +8,61 @@ var verBtn = document.querySelector("#verify-button");
 var identifier = document.querySelector("#q-identifier");
 var qType1 = document.querySelector("#q-type1");
 var qType2 = document.querySelector("#q-type2");
-
-// Get a reference to the database service
-var database = firebase.database();
-//set the level and question vars
-var lvl = "lvl1";
+// retrieve lvl 1 + 2 img
+var lvl1Img;
+var lvl2Img;
+var initLvl1Img = firebase.database().ref("lvl1/img").once('value')
+  .then((snapshot) => {
+    lvl1Img = snapshot.val();
+    console.log("imagesloaded!");
+  })
+  .catch({
+    // failed
+  });
+var initLvl2Img = firebase.database().ref("lvl2/img").once('value')
+  .then((snapshot) => {
+    lvl2Img = snapshot.val();
+  })
+  .catch({
+    // failed
+  });
+// set the level and question vars
+var lvl = 1;
 var qst = 1;
+
+// populate the squares with background images from database
+function populateSquares() {
+  var qstLink;
+  var question = "q" + qst.toString();
+  if (lvl===1){
+    qstLink = lvl1Img[question];
+  }
+  else if (lvl===2){ qstLink = lvl2Img[question]; }
+  qImg.forEach((el) => {
+    // set background
+    el.style.backgroundImage = "url(" + qstLink[el.id] + ")";
+  });
+  // set textContent
+  identifier.textContent = qstLink["identifier"];
+  if (qstLink["type"]==="type1"){
+    qType1.style.display = "inline";
+    qType2.style.display = "none";
+  }
+  else if (qstLink["type"]==="type2"){
+    qType2.style.display = "inline";
+    qType1.style.display = "none";
+  }
+}
 
 // toggle the quiz module
 initBtn.onclick = () => {
   if (quizFrame.style.display === "grid") {
     quizFrame.style.display = "none";
   } else {
+    populateSquares();
     quizFrame.style.display = "grid";
   }
 };
-
-// populate the squares with background images from database
-function populateSquares() {
-  var imgref = lvl + "/img/q" + qst.toString();
-  var images = firebase.database().ref(imgref).once('value')
-    .then((snapshot) => {
-      var snap = snapshot.val();
-      console.log(snap);
-      qImg.forEach((el) => {
-        imgLink = snap[el.id];
-        var thisisit = el.id;
-        // prefetch images
-        const im = new Image();
-        im.src = imgLink;
-        // set background
-        el.style.backgroundImage = "url(" + imgLink + ")"
-        //add to squaresDict
-      });
-      console.log(snap["type"]);
-      identifier.textContent = snap["identifier"];
-      if (snap["type"]==="type1"){
-        qType1.style.display = "inline";
-        qType2.style.display = "none";
-      }
-      else if (snap["type"]==="type2"){
-        qType2.style.display = "inline";
-        qType1.style.display = "none";
-      }
-      console.log(snap["identifier"]);
-
-    })
-    .catch({
-      // failed
-    });
-}
-populateSquares();
 
 // add event listeners to quiz squares
  qImg.forEach((el) => {
@@ -74,11 +81,10 @@ populateSquares();
 // verify button
 verBtn.addEventListener("click", function(){
   qImg.forEach((el) => {
-    if(el.style.margin === "10px"){
-      key = el.id
-      console.log(key);
+    if (el.style.margin === "12px") {
       el.style.margin = "0px";
       el.style.backgroundSize="126px";
+      key = el.id
       // Increment the img score by 1.
       var postRef = firebase.database().ref(lvl+'/scores/q'+qst.toString()+"/"+key);
       postRef.transaction(function(updateScore) {
@@ -91,3 +97,14 @@ verBtn.addEventListener("click", function(){
 })
 
 // DANGER ZONE! RESET ALL SCORES TO ZERO //
+// function resetScores() {
+//   var lvl1Len = lvl1Img;
+//   var lvl2Len = lvl2Img;
+//   console.log(lvl1Len);
+//   console.log(lvl2Len);
+//   var updates = {};
+//   for(x=1; x<10; x++){
+//     updates["lvl1/scores/q1/img0" + x.toString()] = 0;
+//   }
+//   return firebase.database().ref().update(updates);
+// };
