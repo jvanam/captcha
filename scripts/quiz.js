bd24dd413cc2cf41063637ab4bd4c152b7662347
jvanam <jessicavanam@gmail.com>
2020-05-09 11:04:23 -0700

    add identifiers and change phrasing depending on question type

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,12 +2,15 @@ var initBtn = document.querySelector("#b-col1-item");
 var quizFrame = document.querySelector("#q-grid-container");
 var qImg = document.querySelectorAll(".q-img");
 var verBtn = document.querySelector("#verify-button");
+var identifier = document.querySelector("#q-identifier");
+var qType1 = document.querySelector("#q-type1");
+var qType2 = document.querySelector("#q-type2");
 
 // Get a reference to the database service
 var database = firebase.database();
 //set the level and question vars
-var lvl = "lvl1"
-var qst = 1
+var lvl = "lvl1";
+var qst = 1;
 
 // toggle the quiz module
 initBtn.onclick = () => {
@@ -20,10 +23,11 @@ initBtn.onclick = () => {
 
 // populate the squares with background images from database
 function populateSquares() {
-  var imgref = lvl + "/img/q" + qst.toString()
+  var imgref = lvl + "/img/q" + qst.toString();
   var images = firebase.database().ref(imgref).once('value')
     .then((snapshot) => {
       var snap = snapshot.val();
+      console.log(snap);
       qImg.forEach((el) => {
         imgLink = snap[el.id];
         var thisisit = el.id;
@@ -34,6 +38,18 @@ function populateSquares() {
         el.style.backgroundImage = "url(" + imgLink + ")"
         //add to squaresDict
       });
+      console.log(snap["type"]);
+      identifier.textContent = snap["identifier"];
+      if (snap["type"]==="type1"){
+        qType1.style.display = "inline";
+        qType2.style.display = "none";
+      }
+      else if (snap["type"]==="type2"){
+        qType2.style.display = "inline";
+        qType1.style.display = "none";
+      }
+      console.log(snap["identifier"]);
+
     })
     .catch({
       // failed
@@ -60,14 +76,14 @@ verBtn.addEventListener("click", function(){
   qImg.forEach((el) => {
     if(el.style.margin === "10px"){
       key = el.id
+      console.log(key);
+      el.style.margin = "0px";
+      el.style.backgroundSize="126px";
       // Increment the img score by 1.
       var postRef = firebase.database().ref(lvl+'/scores/q'+qst.toString()+"/"+key);
       postRef.transaction(function(updateScore) {
         return updateScore + 1;
       });
-      console.log(key);
-      el.style.margin = "0px";
-      el.style.backgroundSize="126px";
     }
   });
   qst += 1;
